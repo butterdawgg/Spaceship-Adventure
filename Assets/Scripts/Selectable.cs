f9c1d4f5264cdff41caef99bd808feaf85457ede
AndreiPris <47189991+AndreiPris@users.noreply.github.com>
2021-07-23 11:13:19 +0300

    New

---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+   public void Select()
+    {
+       transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+    }
+    public void Deselect()
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+        
+    }
+    
+}
---
+++ Assets/Scripts/ShipDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipDetector : MonoBehaviour
+{
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, transform.forward, Color.yellow);
+
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+ 
+            if(hit.collider.TryGetComponent(out Selectable selectable))
+            {
+                selectable.Select();
+            }
+            else
+            {
+
+            }
+
+        }
+
+    }
+}
