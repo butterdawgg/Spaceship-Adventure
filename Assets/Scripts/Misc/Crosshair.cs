6b7ec94629934e1cbe5324c861ce81fc7ac0dd53
HolyDoggo01 <68026694+HolyDoggo01@users.noreply.github.com>
2021-08-05 22:06:23 +0300

    bugfixes

--- Assets/Scripts/Misc/Crosshair.cs
+++ Assets/Scripts/Misc/Crosshair.cs
@@ -9,7 +9,23 @@ public class Crosshair : MonoBehaviour
     //(Input.mousePosition - arrow.transform.localPosition).normalized
     void Update()
     {
-        arrow.transform.LookAt(Input.mousePosition, -transform.forward);
+        Vector2 mouseDistance;
+        mouseDistance.x = ((Input.mousePosition.x - Screen.width * 0.5f) / Screen.width * 0.5f) * Screen.width;
+        mouseDistance.y = ((Input.mousePosition.y - Screen.height * 0.5f) / Screen.height * 0.5f) * Screen.height;
+
+        Vector3 lookDir = Vector3.zero;
+
+        if (SerializeManager.Instance.GetBool(BoolType.MouseInversionXAxis) & SerializeManager.Instance.GetBool(BoolType.MouseInversionYAxis))
+            lookDir = new Vector3(-mouseDistance.x, -mouseDistance.y);
+        else if (!SerializeManager.Instance.GetBool(BoolType.MouseInversionXAxis) & SerializeManager.Instance.GetBool(BoolType.MouseInversionYAxis))
+            lookDir = new Vector3(mouseDistance.x, -mouseDistance.y);
+        else if (SerializeManager.Instance.GetBool(BoolType.MouseInversionXAxis) & !SerializeManager.Instance.GetBool(BoolType.MouseInversionYAxis))
+            lookDir = new Vector3(-mouseDistance.x, mouseDistance.y);
+        else if (!SerializeManager.Instance.GetBool(BoolType.MouseInversionXAxis) & !SerializeManager.Instance.GetBool(BoolType.MouseInversionYAxis))
+            lookDir = new Vector3(mouseDistance.x, mouseDistance.y);
+
+        arrow.transform.localRotation = Quaternion.LookRotation(lookDir.normalized, -transform.forward);
+
         float distance = (Input.mousePosition - transform.position).magnitude;
         Image arrowImg = arrow.transform.GetChild(0).gameObject.GetComponent<Image>();
         arrowImg.color = new Color(255, 255, 255, distance * 0.002f);
