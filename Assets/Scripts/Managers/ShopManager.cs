adafcd4237fb1cb79681c5ca1b2942241a05d483
HolyDoggo01 <68026694+HolyDoggo01@users.noreply.github.com>
2021-08-06 10:57:52 +0300

    updated weapon models

--- Assets/Scripts/Managers/ShopManager.cs
+++ Assets/Scripts/Managers/ShopManager.cs
@@ -13,18 +13,17 @@ public class ShopManager : MonoBehaviour
 
     [SerializeField] Transform raycast;
 
-    [SerializeField] Text moneyAmount;
+    [SerializeField] Text moneyText;
 
     void Awake()
     {
-        //SerializeManager.Instance.SetFloat(FloatType.Money, 0f);
         shipsButton.onClick.AddListener(OnClickShipsButton);
         gunsButton.onClick.AddListener(OnClickGunsButton);
     }
 
     void Update()
     {
-        moneyAmount.text = SerializeManager.Instance.GetFloat(FloatType.Money).ToString();
+        moneyText.text = "MONEY:" + SerializeManager.Instance.GetFloat(FloatType.Money).ToString();
         
         CheckSelectables();
     }
