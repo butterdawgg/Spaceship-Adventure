9054dffc972c27a17735694ea9f1b1def8c165de
HolyDoggo01 <68026694+HolyDoggo01@users.noreply.github.com>
2021-08-09 13:42:26 +0300

    added more music

--- Assets/Scripts/Guns/PlayerLaserGun.cs
+++ Assets/Scripts/Guns/PlayerLaserGun.cs
@@ -35,24 +35,29 @@ public class PlayerLaserGun : Gun
         Ray ray = new Ray(Player.playerCamera.transform.position, Player.playerCamera.transform.forward);
         if (Physics.Raycast(ray, out RaycastHit hit, 100f, layerMask))
         {
-            //rayEndPoint.position = ray.GetPoint(hit.distance);
-            //hitCollider = hit.collider;
-            //muzzlePoint.LookAt(hit.point);
             Ray ray1 = new Ray(muzzlePoint.position, (ray.GetPoint(hit.distance) - muzzlePoint.position).normalized);
-            if(Physics.Raycast(ray1, out RaycastHit hit1, layerMask))
+            if (Physics.Raycast(ray1, out RaycastHit hit1, layerMask))
             {
-                rayEndPoint.position = ray1.GetPoint(hit1.distance);
+                //rayEndPoint.position = ray1.GetPoint(hit1.distance);
+                lr.SetPosition(1, ray1.GetPoint(hit1.distance));
                 hitCollider = hit1.collider;
+            }
+            else
+            {
+                //rayEndPoint.position = ray.GetPoint(hit.distance);
+                lr.SetPosition(1, ray.GetPoint(hit.distance));
             }
         }
         else
         {
-            rayEndPoint.position = ray.GetPoint(100f);
-            //hitCollider = null;
+            //rayEndPoint.position = ray.GetPoint(100f);
+            lr.SetPosition(1, ray.GetPoint(100f));
         }
 
         lr.SetPosition(0, muzzlePoint.position);
-        lr.SetPosition(1, rayEndPoint.position);
+
+        if (cooldown <= 0 || rayDuration <= 0)
+            lr.SetWidth((Mathf.Sin(Time.time * 10f) * 0.03f) + 0.08f, (Mathf.Sin(Time.time * 10f) * 0.03f) + 0.08f);
     }
 
     private IEnumerator PulseFireCoroutine()
